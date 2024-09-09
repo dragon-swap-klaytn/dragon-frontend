03da15d3c6a39341cf92f71963d237ec268d4087
dragon-swap <dev@dgswap.io>
2024-09-09 16:18:00 +0900

    DragonSwap AutoBuild #13

--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -1,10 +1,10 @@
-import { Address, getAddress } from 'viem'
-import { Currency } from '@pancakeswap/sdk'
 import { ChainId, DEFAULT_CHAIN_ID } from '@pancakeswap/chains'
-import { bsc } from 'wagmi/chains'
-import memoize from 'lodash/memoize'
+import { Currency } from '@pancakeswap/sdk'
 import { TokenAddressMap } from '@pancakeswap/token-lists'
 import { ASSET_CDN } from 'config/constants/endpoints'
+import memoize from 'lodash/memoize'
+import { Address, getAddress } from 'viem'
+import { bsc } from 'wagmi/chains'
 import { chains } from './wagmi'
 
 // returns the checksummed address if the address is valid, otherwise returns undefined
@@ -75,8 +75,8 @@ export function isTokenOnList(defaultTokens: TokenAddressMap<ChainId>, currency?
 export function getChainIcon(chainId: ChainId) {
   return (
     {
-      8217: 'https://klaytn.foundation/wp-content/uploads/2022/12/cropped-favicon-180x180.png',
-      1001: 'https://klaytn.foundation/wp-content/uploads/2022/12/cropped-favicon-180x180.png',
+      8217: 'https://cdn.prod.website-files.com/666642b50954b5d26bc84836/6667a85f241d5c033f976181_KAIA%20256.png',
+      1001: 'https://cdn.prod.website-files.com/666642b50954b5d26bc84836/6667a85f241d5c033f976181_KAIA%20256.png',
     }[chainId] ?? `${ASSET_CDN}/web/chains/${chainId}.png`
   )
 }
