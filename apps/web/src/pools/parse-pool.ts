1bd339952394cf59a8b4c6239470dc4d196b0799
swapscanner-kay <kay@swapscanner.io>
2025-02-17 21:00:13 +0900

    add apr to pools data

--- apps/web/src/pools/parse-pool.ts
+++ apps/web/src/pools/parse-pool.ts
@@ -1,7 +1,7 @@
 import { LP_HOLDERS_FEE } from 'config/constants/info'
 import { PoolV2Detailed, PoolV3Detailed } from 'pools/get-cached-pools-data'
 import { PoolType } from 'types'
-import { calculateAPY } from 'utils/calculate-interests'
+import { calculateAPR, calculateAPY } from 'utils/calculate-interests'
 
 const HOUR = 60 * 60 * 1000
 const DAY = 24 * HOUR
@@ -10,6 +10,10 @@ const WEEK = 7 * DAY
 type WithAPY<T> = T & {
   type: PoolType
   apy: {
+    '24H': number
+    '7D': number
+  }
+  apr: {
     '24H': number
     '7D': number
   }
@@ -28,6 +32,18 @@ export const parseV2Pool = (pool: PoolV2Detailed): WithAPY<PoolV2Detailed> => {
     duration: WEEK,
   })
 
+  const apr24H = calculateAPR({
+    interest: pool.volumeUSD['24H'] * LP_HOLDERS_FEE,
+    principal: pool.tvlUSD.current,
+    duration: DAY,
+  })
+
+  const apr7D = calculateAPR({
+    interest: pool.volumeUSD['7D'] * LP_HOLDERS_FEE,
+    principal: pool.tvlUSD.current,
+    duration: WEEK,
+  })
+
   return {
     type: 'v2',
     ...pool,
@@ -35,18 +51,34 @@ export const parseV2Pool = (pool: PoolV2Detailed): WithAPY<PoolV2Detailed> => {
       '24H': apy24H,
       '7D': apy7D,
     },
+    apr: {
+      '24H': apr24H,
+      '7D': apr7D,
+    },
   }
 }
 
 export const parseV3Pool = (pool: PoolV3Detailed): WithAPY<PoolV3Detailed> => {
   const apy24H = calculateAPY({
-    interest: pool.feeUSD['24H'],
+    interest: pool.feeUSD['24H'] - pool.protocolFeeUSD['24H'],
     principal: pool.tvlUSD.current,
     duration: DAY,
   })
 
   const apy7D = calculateAPY({
-    interest: pool.feeUSD['7D'],
+    interest: pool.feeUSD['7D'] - pool.protocolFeeUSD['7D'],
+    principal: pool.tvlUSD.current,
+    duration: WEEK,
+  })
+
+  const apr24H = calculateAPR({
+    interest: pool.feeUSD['24H'] - pool.protocolFeeUSD['24H'],
+    principal: pool.tvlUSD.current,
+    duration: DAY,
+  })
+
+  const apr7D = calculateAPR({
+    interest: pool.feeUSD['7D'] - pool.protocolFeeUSD['7D'],
     principal: pool.tvlUSD.current,
     duration: WEEK,
   })
@@ -57,6 +89,10 @@ export const parseV3Pool = (pool: PoolV3Detailed): WithAPY<PoolV3Detailed> => {
     apy: {
       '24H': apy24H,
       '7D': apy7D,
+    },
+    apr: {
+      '24H': apr24H,
+      '7D': apr7D,
     },
   }
 }
