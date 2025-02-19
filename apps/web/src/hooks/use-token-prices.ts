8476d1cf11536fcf837b2fa001513b7e2eed5658
swapscanner-kay <kay@swapscanner.io>
2025-02-19 19:08:33 +0900

    use ss kaia price for cake price

--- apps/web/src/hooks/use-token-prices.ts
+++ apps/web/src/hooks/use-token-prices.ts
@@ -8,14 +8,16 @@ const sourceUrls = {
 
 export default function useTokenPrices({
   source = 'default',
+  refreshInterval = 1000 * 30,
 }: {
   source?: 'default' | 'swapscanner'
+  refreshInterval?: number
 } = {}) {
   const { data, error, mutate } = useSWRImmutable<Record<string, number>>(
     sourceUrls[source],
     (key) => fetch(key).then((res) => res.json()),
     {
-      refreshInterval: 1000 * 30,
+      refreshInterval,
     },
   )
 
