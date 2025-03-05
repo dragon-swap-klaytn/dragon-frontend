b325e3ee092db6638b1159948aea4642dc2cec64
swapscanner-kay <kay@swapscanner.io>
2025-03-06 02:05:28 +0900

    fix build

--- apps/web/src/views/Dashboard/utils/wkaiaToKaia.ts
+++ apps/web/src/views/Dashboard/utils/wkaiaToKaia.ts
@@ -1,7 +1,7 @@
 import { WKAIA_ADDRESS } from '@pancakeswap/uikit'
 import { TokenSimple } from 'lib/graph-queries/types'
 
-export function wkaiaToKaia(token: TokenSimple) {
+export function wkaiaToKaia<T extends TokenSimple>(token: T) {
   if (token.id.toLowerCase() === WKAIA_ADDRESS.toLowerCase()) {
     return {
       ...token,
