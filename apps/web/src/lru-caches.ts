2b76c4d3b51605cd69859b159014bd4d86cfdd17
swapscanner-kay <kay@swapscanner.io>
2025-03-29 14:34:17 +0900

    restore pools & tokens lru cache

--- apps/web/src/lru-caches.ts
+++ apps/web/src/lru-caches.ts
@@ -5,10 +5,10 @@ export const blockNumberCache = createLocalLRUCache<number>({ maxSize: 2_048 })
 export type PoolV2AccDataCache = { [id: string]: [number, number, number] }
 export type PoolV3AccDataCache = { [id: string]: [number, number, number, number, number, number] }
 
-// export const v2PoolsAccDataCache = createLocalLRUCache<PoolV2AccDataCache>({ maxSize: 150 })
-// export const v3PoolsAccDataCache = createLocalLRUCache<PoolV3AccDataCache>({ maxSize: 150 })
+export const v2PoolsAccDataCache = createLocalLRUCache<PoolV2AccDataCache>({ maxSize: 150 })
+export const v3PoolsAccDataCache = createLocalLRUCache<PoolV3AccDataCache>({ maxSize: 150 })
 
 export type TokenAccDataCache = { [id: string]: [number, number, number, number, number, number] }
 
-// export const v2TokensAccDataCache = createLocalLRUCache<TokenAccDataCache>({ maxSize: 150 })
-// export const v3TokensAccDataCache = createLocalLRUCache<TokenAccDataCache>({ maxSize: 150 })
+export const v2TokensAccDataCache = createLocalLRUCache<TokenAccDataCache>({ maxSize: 150 })
+export const v3TokensAccDataCache = createLocalLRUCache<TokenAccDataCache>({ maxSize: 150 })
