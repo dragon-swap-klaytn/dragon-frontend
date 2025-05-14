0e227bd10925c14ac021d46d2d9594532a2434e9
swapscanner-kay <kay@swapscanner.io>
2025-05-14 18:03:27 +0900

    log error on /api/pools

--- apps/web/src/pages/api/pools.ts
+++ apps/web/src/pages/api/pools.ts
@@ -95,147 +95,153 @@ const handler: NextApiHandler = async (req, res) => {
   const { types, onlyPoolIds, tokenAddress, boostedOnly, searchKey, sortBy, sortDirection, skip, limit } =
     await poolsSchema.parseAsync(req.query)
 
-  const [{ v2Pools, v3Pools }, prices, ssPrices] = await Promise.all([
-    getCachedPoolsData(),
-    getCachedTokenPrices(),
-    // use swapscanner prices as a fallback
-    getCachedTokenPricesFromSwapscanner().catch((err) => {
-      console.error('/api/pools', err)
-      return {}
-    }),
-  ])
+  try {
+    const [{ v2Pools, v3Pools }, prices, ssPrices] = await Promise.all([
+      getCachedPoolsData(),
+      getCachedTokenPrices(),
+      // use swapscanner prices as a fallback
+      getCachedTokenPricesFromSwapscanner().catch((err) => {
+        console.error('/api/pools', err)
+        return {}
+      }),
+    ])
 
-  const commonPrice = duplicateChecksumPriceMap({ ...ssPrices, ...prices })
+    const commonPrice = duplicateChecksumPriceMap({ ...ssPrices, ...prices })
 
-  const farmFetcherV3 = createFarmFetcherV3(getViemClients)
-  const {
-    farmsWithPrice,
-    cakePerSecond,
-    totalAllocPoint: _,
-  } = await farmFetcherV3.fetchFarms({
-    chainId: ChainId.KLAYTN,
-    farms: farmsV3,
-    commonPrice,
-  })
-
-  const lpAddressToPoolWeights = Object.fromEntries(
-    farmsWithPrice.map((farm) => [lowered(farm.lpAddress), +farm.poolWeight]),
-  )
-
-  if (onlyPoolIds.length > 0) {
-    const onlyPoolIdsLowerCased = onlyPoolIds.map((id) => lowered(id))
-    const missingPoolIds = onlyPoolIdsLowerCased.filter(
-      (id) => !v2Pools.some((pool) => pool.id === id) && !v3Pools.some((pool) => pool.id === id),
-    )
-
-    if (missingPoolIds.length > 0) {
-      const { v2Pools: missingV2Pools, v3Pools: missingV3Pools } = await getPoolsDataByIds(missingPoolIds).catch(
-        (err) => {
-          console.error(`Failed to fetch missing pools: ${missingPoolIds.join(',')}`, err)
-          return { v2Pools: [], v3Pools: [] }
-        },
-      )
-
-      v2Pools.push(...missingV2Pools)
-      v3Pools.push(...missingV3Pools)
-    }
-  }
-
-  const v2PoolsParsed = v2Pools.map(parseV2Pool)
-  const v3PoolsParsed = v3Pools.map((pool) => {
-    const rewardApr = calculateAPR({
-      interest: lpAddressToPoolWeights[pool.id] * +cakePerSecond * prices.KAIA,
-      principal: pool.tvlUSD.current,
-      duration: 1_000,
+    const farmFetcherV3 = createFarmFetcherV3(getViemClients)
+    const {
+      farmsWithPrice,
+      cakePerSecond,
+      totalAllocPoint: _,
+    } = await farmFetcherV3.fetchFarms({
+      chainId: ChainId.KLAYTN,
+      farms: farmsV3,
+      commonPrice,
     })
 
-    return {
-      ...parseV3Pool(pool),
-      rewardApr: Number.isFinite(rewardApr) ? rewardApr : 0,
-    }
-  })
-
-  const filteredV2Pools = filteredByTokenAddress(v2PoolsParsed, tokenAddress).filter((pool) =>
-    filteredBySearchKey(pool, searchKey),
-  )
-  const filteredV3Pools = filteredByTokenAddress(v3PoolsParsed, tokenAddress).filter((pool) =>
-    filteredBySearchKey(pool, searchKey),
-  )
-
-  let pools: PoolParsed[] = []
-
-  if (onlyPoolIds.length > 0) {
-    const onlyPoolIdsLowerCased = onlyPoolIds.map((id) => lowered(id))
-    pools.push(
-      ...filteredV2Pools.filter((pool) => onlyPoolIdsLowerCased.includes(pool.id)),
-      ...filteredV3Pools.filter((pool) => onlyPoolIdsLowerCased.includes(pool.id)),
+    const lpAddressToPoolWeights = Object.fromEntries(
+      farmsWithPrice.map((farm) => [lowered(farm.lpAddress), +farm.poolWeight]),
     )
-  } else {
-    if (types.includes('v2')) {
-      pools.push(...filteredV2Pools)
+
+    if (onlyPoolIds.length > 0) {
+      const onlyPoolIdsLowerCased = onlyPoolIds.map((id) => lowered(id))
+      const missingPoolIds = onlyPoolIdsLowerCased.filter(
+        (id) => !v2Pools.some((pool) => pool.id === id) && !v3Pools.some((pool) => pool.id === id),
+      )
+
+      if (missingPoolIds.length > 0) {
+        const { v2Pools: missingV2Pools, v3Pools: missingV3Pools } = await getPoolsDataByIds(missingPoolIds).catch(
+          (err) => {
+            console.error(`Failed to fetch missing pools: ${missingPoolIds.join(',')}`, err)
+            return { v2Pools: [], v3Pools: [] }
+          },
+        )
+
+        v2Pools.push(...missingV2Pools)
+        v3Pools.push(...missingV3Pools)
+      }
     }
 
-    if (types.includes('v3')) {
-      pools.push(...filteredV3Pools)
-    }
-  }
+    const v2PoolsParsed = v2Pools.map(parseV2Pool)
+    const v3PoolsParsed = v3Pools.map((pool) => {
+      const rewardApr = calculateAPR({
+        interest: lpAddressToPoolWeights[pool.id] * +cakePerSecond * prices.KAIA,
+        principal: pool.tvlUSD.current,
+        duration: 1_000,
+      })
 
-  if (boostedOnly) {
-    pools = pools.filter(
-      (pool) =>
-        lpAddressToPoolWeights[pool.id] > 0 &&
-        (pool as PoolV3Parsed)?.rewardApr &&
-        (pool as PoolV3Parsed).rewardApr > 0,
+      return {
+        ...parseV3Pool(pool),
+        rewardApr: Number.isFinite(rewardApr) ? rewardApr : 0,
+      }
+    })
+
+    const filteredV2Pools = filteredByTokenAddress(v2PoolsParsed, tokenAddress).filter((pool) =>
+      filteredBySearchKey(pool, searchKey),
     )
-  }
+    const filteredV3Pools = filteredByTokenAddress(v3PoolsParsed, tokenAddress).filter((pool) =>
+      filteredBySearchKey(pool, searchKey),
+    )
 
-  const useDesc = sortDirection === 'desc' ? -1 : 1
+    let pools: PoolParsed[] = []
 
-  switch (sortBy) {
-    case 'volume24H': {
-      pools.sort((a, b) => useDesc * ((a.volumeUSD['24H'] ?? 0) - (b.volumeUSD['24H'] ?? 0)))
-      break
-    }
-    case 'volume7D': {
-      pools.sort((a, b) => useDesc * ((a.volumeUSD['7D'] ?? 0) - (b.volumeUSD['7D'] ?? 0)))
-      break
-    }
-    case 'apy24H': {
-      pools.sort(
-        (a, b) =>
-          useDesc *
-          ((a.apy['24H'] ?? 0) +
-            ((a as PoolV3Parsed).rewardApr ?? 0) -
-            (b.apy['24H'] ?? 0) -
-            ((b as PoolV3Parsed).rewardApr ?? 0)),
+    if (onlyPoolIds.length > 0) {
+      const onlyPoolIdsLowerCased = onlyPoolIds.map((id) => lowered(id))
+      pools.push(
+        ...filteredV2Pools.filter((pool) => onlyPoolIdsLowerCased.includes(pool.id)),
+        ...filteredV3Pools.filter((pool) => onlyPoolIdsLowerCased.includes(pool.id)),
       )
-      break
-    }
-    case 'apy7D': {
-      pools.sort(
-        (a, b) =>
-          useDesc *
-          ((a.apy['7D'] ?? 0) +
-            ((a as PoolV3Parsed).rewardApr ?? 0) -
-            (b.apy['7D'] ?? 0) -
-            ((b as PoolV3Parsed).rewardApr ?? 0)),
-      )
-      break
-    }
-    case 'tvl': {
-      pools.sort((a, b) => useDesc * (a.tvlUSD.current - b.tvlUSD.current))
-      break
-    }
-    default: {
-      break
-    }
-  }
+    } else {
+      if (types.includes('v2')) {
+        pools.push(...filteredV2Pools)
+      }
 
-  res.status(200).json({
-    pools: pools.slice(skip, skip + limit),
-    totalPage: Math.ceil(pools.length / limit),
-    totalCount: pools.length,
-  })
+      if (types.includes('v3')) {
+        pools.push(...filteredV3Pools)
+      }
+    }
+
+    if (boostedOnly) {
+      pools = pools.filter(
+        (pool) =>
+          lpAddressToPoolWeights[pool.id] > 0 &&
+          (pool as PoolV3Parsed)?.rewardApr &&
+          (pool as PoolV3Parsed).rewardApr > 0,
+      )
+    }
+
+    const useDesc = sortDirection === 'desc' ? -1 : 1
+
+    switch (sortBy) {
+      case 'volume24H': {
+        pools.sort((a, b) => useDesc * ((a.volumeUSD['24H'] ?? 0) - (b.volumeUSD['24H'] ?? 0)))
+        break
+      }
+      case 'volume7D': {
+        pools.sort((a, b) => useDesc * ((a.volumeUSD['7D'] ?? 0) - (b.volumeUSD['7D'] ?? 0)))
+        break
+      }
+      case 'apy24H': {
+        pools.sort(
+          (a, b) =>
+            useDesc *
+            ((a.apy['24H'] ?? 0) +
+              ((a as PoolV3Parsed).rewardApr ?? 0) -
+              (b.apy['24H'] ?? 0) -
+              ((b as PoolV3Parsed).rewardApr ?? 0)),
+        )
+        break
+      }
+      case 'apy7D': {
+        pools.sort(
+          (a, b) =>
+            useDesc *
+            ((a.apy['7D'] ?? 0) +
+              ((a as PoolV3Parsed).rewardApr ?? 0) -
+              (b.apy['7D'] ?? 0) -
+              ((b as PoolV3Parsed).rewardApr ?? 0)),
+        )
+        break
+      }
+      case 'tvl': {
+        pools.sort((a, b) => useDesc * (a.tvlUSD.current - b.tvlUSD.current))
+        break
+      }
+      default: {
+        break
+      }
+    }
+
+    res.status(200).json({
+      pools: pools.slice(skip, skip + limit),
+      totalPage: Math.ceil(pools.length / limit),
+      totalCount: pools.length,
+    })
+  } catch (err) {
+    console.error('/api/pools', err)
+
+    throw err
+  }
 }
 
 export default handler
