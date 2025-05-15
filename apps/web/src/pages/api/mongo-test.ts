1af242998a5ed34e84adb4ea6e3b893578822348
swapscanner-kay <kay@swapscanner.io>
2025-05-15 19:20:20 +0900

    mongodb test 8

--- apps/web/src/pages/api/mongo-test.ts
+++ apps/web/src/pages/api/mongo-test.ts
@@ -5,15 +5,11 @@ const handler: NextApiHandler = async (req, res) => {
   console.log(process.env)
   console.log('========================================')
 
-  const useMongoCache = process.env.USE_MONGO_CACHE ?? null
-  const mongodbUri = process.env.MONGODB ?? null
   const nextPublicGraph = process.env.NEXT_PUBLIC_DGSWAP_GATEWAY ?? null
   const graph = process.env.NEXT_DGSWAP_GATEWAY ?? null
   const walletConnectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_ID ?? null
 
   return res.json({
-    useMongoCache,
-    mongodbUri,
     nextPublicGraph,
     graph,
     walletConnectId,
