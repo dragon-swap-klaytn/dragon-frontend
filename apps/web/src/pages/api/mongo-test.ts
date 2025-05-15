651f6ad0d208e9550ca5ab4df79a12b5055b5784
swapscanner-kay <kay@swapscanner.io>
2025-05-15 17:30:39 +0900

    mongodb test2

--- apps/web/src/pages/api/mongo-test.ts
+++ apps/web/src/pages/api/mongo-test.ts
@@ -1,12 +1,16 @@
 import { NextApiHandler } from 'next'
 
 const handler: NextApiHandler = async (req, res) => {
-  const useMongoCache = process.env.USE_MONGO_CACHE
-  const mongodbUri = process.env.MONGODB
+  const useMongoCache = process.env.USE_MONGO_CACHE ?? null
+  const mongodbUri = process.env.MONGODB ?? null
+  const nextPublicGraph = process.env.NEXT_PUBLIC_DGSWAP_GATEWAY ?? null
+  const graph = process.env.NEXT_DGSWAP_GATEWAY ?? null
 
   return res.json({
     useMongoCache,
     mongodbUri,
+    nextPublicGraph,
+    graph,
   })
 }
 
