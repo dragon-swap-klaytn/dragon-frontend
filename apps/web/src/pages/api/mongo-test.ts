0e564d99cd62b10c500963ba3a4328af27621b5c
swapscanner-kay <kay@swapscanner.io>
2025-05-15 17:56:34 +0900

    mongodb test3

--- apps/web/src/pages/api/mongo-test.ts
+++ apps/web/src/pages/api/mongo-test.ts
@@ -1,6 +1,10 @@
 import { NextApiHandler } from 'next'
 
 const handler: NextApiHandler = async (req, res) => {
+  console.log('=== Environment Variables in API Route ===')
+  console.log(process.env)
+  console.log('========================================')
+
   const useMongoCache = process.env.USE_MONGO_CACHE ?? null
   const mongodbUri = process.env.MONGODB ?? null
   const nextPublicGraph = process.env.NEXT_PUBLIC_DGSWAP_GATEWAY ?? null
