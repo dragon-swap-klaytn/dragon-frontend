f19c7fed527cc1af5bbbea6cec2cf1c648a5e5e6
daniel <daniel@swapscanner.io>
2025-05-25 17:35:01 +0900

    use-on-chain-data-if-filteredTokenMap-is-empty

--- apps/web/src/hooks/Tokens.ts
+++ apps/web/src/hooks/Tokens.ts
@@ -186,6 +186,10 @@ export function useTokens(searchKey?: string) {
         })
       }
 
+      if (filteredTokenMap.size === 0 && data) {
+        return [new Token(chainId, data.address, data.decimals, data.symbol ?? 'UNKNOWN', data.name ?? 'Unknown Token')]
+      }
+
       return Array.from(filteredTokenMap.values()).map(
         (t) => new Token(chainId, t.address, t.decimals, t.symbol ?? 'UNKNOWN', t.name ?? 'Unknown Token'),
       )
