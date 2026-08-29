7ccf8a4e54cd2bd947f7ee839df487a9a7b06cdf
daniel <daniel@swapscanner.io>
2026-08-29 18:03:37 +0900

    web: add comment to clarify IDRP Token in hidden token addresses

--- apps/web/src/components/SearchModal/hiddenTokens.ts
+++ apps/web/src/components/SearchModal/hiddenTokens.ts
@@ -1,5 +1,5 @@
 const HIDDEN_TOKEN_ADDRESSES = new Set(
-  ['0xC16d986585407A74Ab87d17C3d0Dc19822E3EB35'].map((address) => address.toLowerCase()),
+  ['0xC16d986585407A74Ab87d17C3d0Dc19822E3EB35'].map((address) => address.toLowerCase()), // IDRP Token
 )
 
 export const isHiddenTokenAddress = (address?: string | null) => {
